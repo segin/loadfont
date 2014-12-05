961f4c0cb3098ba91646d50df83a35c12d4d0348
Kirn Gill II <segin2005@gmail.com>
2014-12-05 11:54:09 -0600

    Added loadfont.c as example code.

---
+++ loadfont.c
@@ -0,0 +1,30 @@
+/* loadfont - Utility to temporarily load a font on Windows.
+ * Copyright (C) 2014 Kirn Gill <segin2005@gmail.com>
+ * 
+ * vim: ts=4 sw=4 ai
+ * 
+ * See LICENSE for copyright licencing details. 
+ */ 
+
+#include <windows.h>
+#include <stdio.h>
+#include <shellapi.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	LPWSTR *arglist;
+	int nargs;
+	int i;
+
+	arglist = CommandLineToArgvW(GetCommandLineW(), &nargs);
+	if(arglist == NULL) { 
+		wprintf(L"CommandLineToArgvW failed.\n");
+		return(1);
+	} else { 
+		for(i = 0; i < nargs; i++) { 
+			printf("%d: %ws\n", i, arglist[i]);
+		};
+	}
+	LocalFree(arglist);
+	return(0);
+}
